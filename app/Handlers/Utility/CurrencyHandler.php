9aa4013374d9825829b01d894e7aeb5f25830fdf
Andreas Elia <andreaselia@live.co.uk>
2016-10-15 00:29:32 +0100

    Progress on currency handler

--- app/Handlers/Utility/CurrencyHandler.php
+++ app/Handlers/Utility/CurrencyHandler.php
@@ -7,6 +7,7 @@ use App\Slack\Message;
 use App\Handlers\Handler;
 use App\Loader\LoadsData;
 use App\Loader\JsonLoader;
+use Symfony\Component\Intl\Intl;
 use Illuminate\Support\Collection;
 
 final class CurrencyHandler extends Handler
@@ -44,11 +45,29 @@ final class CurrencyHandler extends Handler
 
         $arguments = $this->arguments($event);
 
+        $rates = $this->data['rates'];
+
+        $result = round($arguments[0] * $rates[strtoupper($arguments[2])], 2);
+
+        $symbol = Intl::getCurrencyBundle()->getCurrencySymbol('USD');
+
         $this->send(
-            Message::saying('`conversion in progress' . $this->joinArguments($arguments) . '`')
+            Message::saying(
+                "<@{$event->sender()}> " . $this->symbol($arguments[1]) . strtoupper($arguments[0]) . " is around " . $this->symbol($arguments[2]) . strtoupper($result)
+            )
             ->inChannel($event->channel())
             ->to($event->sender())
         );
+    }
+
+    /**
+     * @param  string $currency
+     *
+     * @return string
+     */
+    private function symbol(string $currency)
+    {
+        return Intl::getCurrencyBundle()->getCurrencySymbol(strtoupper($currency));
     }
 
     /**
