59d41551809476d63ebd6cf59cd983260feddb74
Andreas Elia <andreaselia@live.co.uk>
2016-10-16 14:46:40 +0100

    Changes to the currency handler command

--- app/Handlers/Utility/CurrencyHandler.php
+++ app/Handlers/Utility/CurrencyHandler.php
@@ -44,23 +44,26 @@ final class CurrencyHandler extends Handler
         $this->loadData();
 
         $arguments = $this->arguments($event);
-        $content   = '`@eve convert 10 GPB to USD` or `@eve convert £10 to $`';
+        $content   = '`@eve convert 10 GPB to USD`';
 
-        // convert 10 GBP to USD
         if ($arguments->count() == 4) {
-            $rates   = $this->data['rates'];
-            $result  = round($arguments[0] * $rates[strtoupper($arguments[3])], 2);
-            $symbol  = Intl::getCurrencyBundle()->getCurrencySymbol('USD');
-            $content = "<@{$event->sender()}> " . $this->symbol($arguments[1]) . strtoupper($arguments[0]) . " is around " . $this->symbol($arguments[3]) . strtoupper($result);
-        }
-
-        // convert £10 to $
-        if ($arguments->count() == 3) {
-            $content = "<@{$event->sender()}> " . $arguments[0] . " is around ";
+            if (preg_match('/[A-Z]{3}/', $arguments[1]) && trim($arguments[2] == 'to') && preg_match('/[A-Z]{3}/', $arguments[3])) {
+                $rates  = $this->data['rates'];
+                $result = round($arguments[0] * $rates[strtoupper($arguments[3])], 2);
+                $symbol = Intl::getCurrencyBundle()->getCurrencySymbol('USD');
+                $content = sprintf(
+                    "<@%s> %s%s is around %s%s",
+                    $event->sender(),
+                    $this->symbol($arguments[1]),
+                    strtoupper($arguments[0]),
+                    $this->symbol($arguments[3]),
+                    strtoupper($result)
+                );
+            }
         }
 
         $this->send(
-            Message::saying($content . json_encode($arguments))
+            Message::saying($content)
             ->inChannel($event->channel())
             ->to($event->sender())
         );
@@ -84,28 +87,11 @@ final class CurrencyHandler extends Handler
     private function arguments(Event $event)
     {
         preg_match_all(
-            '/\s+/',
+            '/([\w]+)/',
             substr($event->text(), strpos($event->text(), 'convert ') + 8),
             $matches
         );
 
         return collect($matches[0])->unique();
-    }
-
-
-    /**
-     * @param Collection $arguments
-     *
-     * @return string
-     */
-    private function joinArguments(Collection $arguments)
-    {
-        $last = $arguments->pop();
-
-        if (! $arguments->isEmpty()) {
-            return $arguments->implode(', ') . ' and ' . $last;
-        }
-
-        return $last;
     }
 }
